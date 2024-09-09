606d08fa2a1037001752ee8d9248cf238d794769
Ameerah Hussain <ameerahussain7860@gmail.com>
2024-09-09 21:45:37 +0530

    git setup

--- src/hospital/management/system/patient_discharge.java
+++ src/hospital/management/system/patient_discharge.java
@@ -2,6 +2,8 @@ package hospital.management.system;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.ResultSet;
 import java.util.Date;
 
@@ -26,24 +28,20 @@ public class patient_discharge extends JFrame {
         label2.setForeground(Color.white);
         panel.add(label2);
 
-
-        //ameerah//
         Choice choice = new Choice();
         choice.setBounds(200,80,150,25);
         panel.add(choice);
 
-        try {
+        try{
             conn c = new conn();
-            ResultSet resultSet=c.statement.executeQuery("select * from Patient_Info");
+            ResultSet resultSet = c.statement.executeQuery("select * from Patient_Info");
             while (resultSet.next()){
-                choice.add(resultSet.getString("Number"));
+                choice.add(resultSet.getString("number"));
             }
+
         }catch (Exception e){
             e.printStackTrace();
         }
-
-
-
 
         JLabel label3 = new JLabel("Room Number");
         label3.setBounds(30,130,150,20);
@@ -88,24 +86,65 @@ public class patient_discharge extends JFrame {
         discharge.setBackground(Color.black);
         discharge.setForeground(Color.white);
         panel.add(discharge);
+        discharge.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                conn c = new conn();
+                try {
+                    c.statement.executeUpdate("delete from Patient_Info where number = '"+choice.getSelectedItem()+"'");
+                    c.statement.executeUpdate("update room set Availability = 'Available' where room_no = '"+RNo.getText()+"'");
+                    JOptionPane.showMessageDialog(null,"Done");
+                    setVisible(false);
+                }catch (Exception E){
+                    E.printStackTrace();
+                }
 
 
+            }
+        });
+
+        JButton Check = new JButton("Check");
+        Check.setBounds(170,300,120,30);
+        Check.setBackground(Color.black);
+        Check.setForeground(Color.white);
+        panel.add(Check);
+        Check.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                conn c = new conn();
+                try{
+                    ResultSet resultSet = c.statement.executeQuery("select * from Patient_Info where number = '"+choice.getSelectedItem()+"'");
+                    while (resultSet.next()){
+                        RNo.setText(resultSet.getString("Room_Number"));
+                        INTime.setText(resultSet.getString("Time"));
+                    }
+                }catch (Exception E){
+                    E.printStackTrace();
+                }
+            }
+        });
+
+        JButton Back = new JButton("Back");
+        Back.setBounds(300,300,120,30);
+        Back.setBackground(Color.black);
+        Back.setForeground(Color.white);
+        panel.add(Back);
+        Back.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setVisible(false);
+            }
+        });
 
 
-
-
+        setUndecorated(true);
         setSize(800,400);
         setLayout(null);
         setLocation(400,250);
         setVisible(true);
 
-
-
-
-
     }
     public static void main(String[] args) {
         new patient_discharge();
-
     }
 }
